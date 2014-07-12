85b782897a9864b225dccc04fec66be83b575acd
Mateusz Klimek <matkk@codility.com>
2014-07-12 17:15:04 +0200

    add new bike method

--- bikeshare/bikeshare/payments/urls.py
+++ bikeshare/bikeshare/payments/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('bikeshare.payments.views',
     url(r'^get_token/$', 'get_token', name='get_token'),
+    url(r'^new_bike/$', 'new_bike', name='new_bike'),
 )
--- bikeshare/bikeshare/payments/views.py
+++ bikeshare/bikeshare/payments/views.py
@@ -14,3 +14,15 @@ def get_token(request):
     response = {}
     response["client_token"] = client_token
     return HttpResponse(json.dumps(response), content_type="application/json")
+
+
+def new_bike(request):
+    nonce = request.GET.get('payment_method_nonce')
+    result = braintree.Transaction.sale({
+        "amount": "10.00",
+        "payment_method_nonce": nonce
+    })
+    response = {}
+    response["success"] = result.is_success
+    response["message"] = result.message
+    return HttpResponse(json.dumps(response), content_type="application/json")
